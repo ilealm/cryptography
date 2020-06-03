993e92343667805711b5bf0f9b827fed98393aef
Iris Leal <iris_leal_m@hotmail.com>
2020-06-03 13:29:46 -0700

    Encrypt function ready

--- cryptography.py
+++ cryptography.py
@@ -5,6 +5,10 @@ def getAlfabethPossition(ch):
     position = alfabeth.index(ch) #stars cointing on 0
     return position
 
+def getAlfabethLetter(index):
+    alfabeth = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'           
+    return alfabeth[index]
+
 
 def encrypt(plain, key):
   # 'Iris Leal is my name 41'
@@ -21,16 +25,13 @@ def encrypt(plain, key):
         else:
             # if is a letter: get the position on the alphabet
             alphabet_possition = getAlfabethPossition(ch)  # stars on 0
-            # print('Position of ', ch, ' is ', alphabet_possition)
             # use mod to get the value of the new letter
             new_possition = (alphabet_possition + int(key)) % 26
-            # print (new_possition)
-            print('Position of ', ch, ' is ', alphabet_possition, ' new position: ', new_possition)
             # get the letter on that position
+            new_letter = getAlfabethLetter(new_possition)
             # add to the final message
-        encrypt_msg += ch
-  # if isn't a letteradd to encrypt_msg
-
+            encrypt_msg += new_letter
+            # print('Position of ', ch, ' is ', alphabet_possition, ' new position: ', new_possition, ' new_letter: ', new_letter)
 
     return encrypt_msg
 
@@ -43,8 +44,8 @@ def decryp(plainkey= None):
 
 
 if __name__ == "__main__":
-    # text = 'Iris Leal is my name 41'
-    text = 'ABCDEFG'
-    # text = 'VWXYZ'
-    key=3
+    text = 'Iris Leal is my name 41'
+    # text = 'ABCDEFG'
+    # text = 'I'
+    key= 3
     print (encrypt(text,key))
