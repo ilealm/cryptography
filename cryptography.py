1b5c59b14fd5c5e1c88cd68c66d9ada4b3db5790
Iris Leal <iris_leal_m@hotmail.com>
2020-06-03 15:23:15 -0700

    First working draft crack_code function

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,11 @@
+import nltk
+nltk.download('words')
+from nltk.corpus import words
+
 def getAlfabethPossition(ch):
     alfabeth = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     
-    position = alfabeth.index(ch) #stars cointing on 0
+    position = alfabeth.index(ch) # starts cointing on 0
     return position
 
 def getAlfabethLetter(index):
@@ -29,19 +33,57 @@ def encrypt(plain, key):
 
 
 
-def decryp(plain, key= None):
+def decryp(plain, key):
     if key:
-      return encrypt( plain, -key)
+        return encrypt( plain, -key)
+
+
+def crack_code(plain):
+    word_list = words.words()
+    # print(word_list[0:10])
+    key_with_max_words = 0
+    max_words_found = 0
+    plain_words = plain.split(' ')
+
+    for index in range (26):
+        # this is the counter of words in this round
+        founded_words_per_index = 0
+        for word in plain_words:
+            decryp_word = decryp(word, index)
+            if decryp_word in word_list: 
+                founded_words_per_index += 1
+                print('I found this word in the list', decryp_word, ' in round ', index)
+        if founded_words_per_index > max_words_found:
+            print('Change values from ', founded_words_per_index, ' to ', max_words_found)
+            max_words_found = founded_words_per_index
+            key_with_max_words = index
+            print('NEW values from ', founded_words_per_index, ' to ', max_words_found, ' index: ', index)
+
+
+    # i know i can have move 26 switchs
+
+    # for each word, i will create a new word moving from 0-26 Position
+    # i will see if the word exist, if so, i will have a words_existing_count for each iteration
+
+    # will return the iterations with more words existing 
+        # word_list = words.words()
+        # print(word_list[0:10])
+    print('key_with_max_words ', key_with_max_words)
+    return decryp(plain,key_with_max_words)
 
 
 
 if __name__ == "__main__":
-    text = 'Iris Leal is my name 41 xyz'
+    # text = 'Iris Leal is my name 41 xyz'
     # text = 'ABCDEFG'
     # text = 'I'
+    text = 'It was the best of times, it was the worst of times.'
     key= 13
     code = encrypt(text,key)
-    print(text)
-    print (code )
-    decode = decryp(code, key)
-    print (decode)
+    # print(text)
+    print('the code is: ', code )
+    # decode = decryp(code, key)
+    # print (decode)
+    # print(decryp(code))
+    print(crack_code(code))
+
