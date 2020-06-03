fb3e5c6fb1859214c0589f80599a5654ee8574b5
Iris Leal <iris_leal_m@hotmail.com>
2020-06-03 13:35:35 -0700

    Decryp function ready

--- cryptography.py
+++ cryptography.py
@@ -10,26 +10,19 @@ def getAlfabethLetter(index):
     return alfabeth[index]
 
 
-def encrypt(plain, key):
-  # 'Iris Leal is my name 41'
+def encrypt(plain, key):  
     encrypt_msg = ''
     if not plain or not key:
         raise Exception("The text and the key must be supplied.")
         return ""
 
-  # i need to take each character of the string
     for ch in plain:
-        # print(ch)
         if not ch.isalpha():
             encrypt_msg += ch
         else:
-            # if is a letter: get the position on the alphabet
             alphabet_possition = getAlfabethPossition(ch)  # stars on 0
-            # use mod to get the value of the new letter
             new_possition = (alphabet_possition + int(key)) % 26
-            # get the letter on that position
             new_letter = getAlfabethLetter(new_possition)
-            # add to the final message
             encrypt_msg += new_letter
             # print('Position of ', ch, ' is ', alphabet_possition, ' new position: ', new_possition, ' new_letter: ', new_letter)
 
@@ -37,10 +30,10 @@ def encrypt(plain, key):
 
 
 
-def decryp(plainkey= None):
+def decryp(plain, key= None):
+    if key:
+      return encrypt( plain, -key)
 
-    # if key = Nonetry to decrypt another way
-    pass
 
 
 if __name__ == "__main__":
@@ -48,4 +41,8 @@ if __name__ == "__main__":
     # text = 'ABCDEFG'
     # text = 'I'
     key= 3
-    print (encrypt(text,key))
+    code = encrypt(text,key)
+    print(text)
+    print (code )
+    decode = decryp(code, key)
+    print (decode)
